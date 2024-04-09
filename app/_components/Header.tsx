52834c41126847e05875b6dc695c88d4000a71fd
Cyprien <cyprien@adalgo.fr>
2024-04-09 07:59:06 +0200

    change hearder

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
   return (
     <header className="sticky top-0 py-4">
       <Section className="flex items-baseline">
-        <h1 className="text-lg font-bold text-primary">cyprienf.com</h1>
+        <h1 className="text-lg font-bold text-primary">cyprienfiquet.com</h1>
         <div className="flex-1" />
         <ul className="flex items-center gap-2">
           <Link
